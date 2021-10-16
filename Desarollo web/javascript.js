790c147211ec21634bf8cc14e9a208b6b7ef4e83
Ignacio-engine-master <80260820+Ignacio-engine-master@users.noreply.github.com>
2021-10-16 20:49:43 -0300

    Add files via upload

--- Desarollo web/javascript.js
+++ Desarollo web/javascript.js
@@ -1,29 +1,30 @@
-const a = [
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
+const words = [
+  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
   {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
-  {letra: "b" , descripcion : "Tu hermana " , respuesta: "hermana"},
-  {letra: "a" , descripcion : "Tu hermana sin a " , respuesta: "hermn"},
+  {letra: "c" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "d" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "e" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "f" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "g" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "h" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "i" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "j" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "k" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "m" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "n" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "ñ" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "o" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "p" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "q" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "r" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "s" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "t" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "u" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "v" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "w" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
+  {letra: "x" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "y" , descripcion : "Tu hermana " , respuesta: "hermana"},
+  {letra: "z" , descripcion : "Tu hermana sin a " , respuesta: "hermana"},
 ];
 
 const rosco = document.getElementById('rosco');
@@ -31,17 +32,45 @@ console.log(rosco);
 
 let index = 0;
 console.log(rosco.children[index]);
+/** FUNCTIONS */
 
+/**CUANDO APRETAS 'JUGA' TE MANDA AL ROSCO DIRECTAMENTE */
+function logear() {
+  container.remove();
+  princi.style.visibility = 'visible';
+  
+}
+/**BORRA EL FORMULARIO Y APARECE EL PASAPALABRA */
+function back() {
+  formulario.remove();
+  princi.style.visibility = 'visible';
+  
+}
 
-function c() {
-
+/** APRETAS REGISTRARSE Y TE REDIRECCIONA AL FORMULARIO */
+function change() {
+  container.remove();
+  formulario.style.visibility = 'visible';
+  
+}
+/** ES UNA """ANIMACION""" CON EL TEXTO DEL FORMULARIO */
+function keyup() {
+  if (this.value.length >= 1) {
+    this.nextElementSibling.classList.add('focus');
+  }
+  else {
+    this.nextElementSibling.classList.remove('focus');
+  }
+}
+ /**EL PASAPALABRA */
+function skip() {
   rosco.children[index].classList.toggle('letra--skip');
   index++;
 }
-
-function e() {
+/** VERIFICA SI TA BOM THE ASNWER */
+function verificar() {
   const inputEnviar = document.getElementById('inputEnviar').value;
-  if (inputEnviar == a[index].respuesta) {
+  if (inputEnviar == words[index].respuesta) {
     rosco.children[index].classList.toggle('letra--approve'); 
   }else{
     rosco.children[index].classList.toggle('letra--fail');
@@ -49,25 +78,24 @@ function e() {
   index++;
 console.log(index);
 }
+/** FUNCTIONS */
 
-
-
-
-
-
-
-
-
-
-
-
-function logear() {
-    princi.style.visibility = 'visible';
-    container.remove();
-  }
 
 
   /** COSAS PARA CAMBIAR de pagina */
+var formulario = document.getElementById('formulario')
 var princi = document.getElementById('princi');
 var container = document.getElementById('container');
+var register = document.getElementById('btnRegister')
+var redi = document.getElementById('BtnBack')
 /** COSAS PARA CAMBIAR de pagina */
+
+
+/** FORMULARIO DE LOGEO */
+var input = document.getElementsByClassName('FormInput');
+for (var r = 0; r < input.length; r++) {
+  input[i].addEventListener(keyup)
+}
+/** FORMULARIO DE LOGEO */
+
+
