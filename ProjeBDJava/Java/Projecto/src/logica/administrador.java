4154a700144db4e51ca63b65930374b5816522cf
facundo perez <67847013+facuft2@users.noreply.github.com>
2021-11-13 16:38:18 -0300

    Delete ProjeBDJava directory

--- ProjeBDJava/Java/Projecto/src/logica/administrador.java
+++
@@ -1,9 +0,0 @@
-package logica;
-
-public class administrador extends usuario {
-
-	public administrador(int ci, String nombre, String apellido, String mail, int pin) {
-		super(ci, nombre, apellido, mail, pin);
-	}
-	
-}
